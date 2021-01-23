b9a9eb9e86ed5f578e445061e319d1495e4e34ba
Mohamed Ibrahim Mohamed Yossef <49948244+MohamedIbrahim13@users.noreply.github.com>
2021-01-23 09:35:17 +0200

    update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const schema = require('./schema/Schema');
 
 app.use(cors());
 
-mongoose.connect('mongodb+srv://test:test@cluster0.9fdg8.mongodb.net/graphqldb?retryWrites=true&w=majority',{useUnifiedTopology:true,useNewUrlParser:true}).then((result)=>{
+mongoose.connect('',{useUnifiedTopology:true,useNewUrlParser:true}).then((result)=>{
     app.listen(4000,()=>{
         console.log('Server is running with the database...')
     });
